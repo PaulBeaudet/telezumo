c31e4812917f36e9d950a45f3f98afe21cb59f43
Paul Beaudet <inof8or@gmail.com>
2016-09-25 20:44:20 -0400

    here's to testing in production

--- views/js/remote.js
+++ views/js/remote.js
@@ -33,9 +33,9 @@ var control = {
         $('#speed4').on('click', function(){control.send('remote', 'S4');});
         $('#horn'  ).on('click', function(){control.send('remote', 'C1');});
         $('#offsetSubmit').on('click', function(){
-            var offset = String.fromCharCode($(#offsetValue).val());
+            var offset = String.fromCharCode( $('#offsetValue').val() );
             var command = 'O' + offset;
-            console.log('sending command:' + command);
+            console.log('sending command' + command);
             control.send('remote', command);
         });
         $('#disconnect').on('click touchstart', control.disconnect);
